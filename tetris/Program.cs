cd4e5624ce3bfe74635341c4d5fd2b99f89f3230
xeonx86 <xeonx86@gmail.com>
2020-12-13 19:19:03 +0300

    вывели 2 точки на экран

--- tetris/Program.cs
+++ tetris/Program.cs
@@ -6,7 +6,23 @@ namespace tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+            int x1, y1;
+            char ch = '*';
+            x1 = 3;
+            y1 = 4;
+            int x2 = 5;
+            int y2 = 4;
+            Drow(x1, y1, ch);
+            Drow(x2, y2, ch);
+            Console.ReadLine();
+        }
+
+        static void Drow(int x,int y, char ch)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(ch);
         }
     }
 }
