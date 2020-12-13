ad767d40b395c32cbb17fbbf032687aa9b815dd1
xeonx86 <xeonx86@gmail.com>
2020-12-13 19:00:40 +0300

    added !!!

--- tetris/Program.cs
+++ tetris/Program.cs
@@ -6,7 +6,7 @@ namespace tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!!");
         }
     }
 }
