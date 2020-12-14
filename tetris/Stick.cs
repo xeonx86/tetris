1a964234f5a34fc4e355c53701cb7933e05b321d
xeonx86 <xeonx86@gmail.com>
2020-12-14 11:18:16 +0300

    Передобавлены squre и Stick

---
+++ tetris/Stick.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tetris
+{
+    class Stick
+    {
+        Point[] points = new Point[4];
+
+        public Stick(int x, int y, char ch)
+        {
+            points[0] = new Point(x, y, ch);
+            points[1] = new Point(x, y+1, ch);
+            points[2] = new Point(x, y+2, ch);
+            points[3] = new Point(x, y+3, ch);
+        }
+
+        public void Drow()
+        {
+            foreach (Point p in points)
+                p.Draw();
+        }
+    }
+}
---
+++ tetris/sDraw.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tetris
+{
+    class squreDraw
+    {
+
+        Point[] points = new Point[4];
+
+        public squreDraw(int x, int y, char sym)
+        {
+            points[0] = new Point(x, y, sym);
+            points[1] = new Point(x+1, y, sym);
+            points[2] = new Point(x, y+1, sym);
+            points[3] = new Point(x+1, y+1, sym);
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in points)
+                p.Draw();
+        }
+    }
+}
